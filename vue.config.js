da24b9754c8fb97fd34b1bc4a4818c523d104719
yocn <yoc.yocn@gmail.com>
2024-01-24 10:55:01 +0900

    vueのバージョンを変更

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-    devServer: {
-        disableHostCheck: true,
-        port: 8080,
-        public: '0.0.0.0:8080'
+  devServer: {
+    // disableHostCheck: true,
+    // port: 8080,
+    // public: '0.0.0.0:8080'
+    // port: 8080,
+    // contentBase: path.resolve(__dirname, 'public'),
+    // host: 'localhost',
+    // client: {
+    //   host: '0.0.0.0',
+    //   webSocketURL: 'ws://0.0.0.0:8080/ws',
+    // },
+    // public: '0.0.0.0:8080',
+    client: {
+      webSocketURL: 'ws://0.0.0.0:8080/ws',
     },
-    publicPath: "./"
-}
+  },
+  publicPath: './',
+};
