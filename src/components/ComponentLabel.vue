db40f23513eadce3e4ecbf74ca0f58fed37c861f
yocn <yoc.yocn@gmail.com>
2024-01-24 16:28:26 +0900

    完成形

--- src/components/ComponentLabel.vue
+++ src/components/ComponentLabel.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="component">
-    <div ref="inputLabel" contenteditable="true" v-text="label" @blur="changeLabel"></div>
+    <div
+      ref="inputLabel"
+      contenteditable="true"
+      v-text="label"
+      @input="changeLabel"
+      @compositionstart="mutateComposition($event, 'start')"
+      @compositionend="mutateComposition($event, 'end')"
+    ></div>
   </div>
 </template>
 
@@ -8,6 +15,11 @@
 import { mapGetters } from 'vuex';
 
 export default {
+  data() {
+    return {
+      isCompositioning: false,
+    };
+  },
   computed: {
     ...mapGetters({
       label: 'getLabel',
@@ -16,8 +28,34 @@ export default {
   methods: {
     changeLabel(event) {
       let updateLabel = event.target.textContent.trim();
+      if (this.isCompositioning || updateLabel.length === 0) {
+        return;
+      }
+
+      // commitするとカーソルが先頭に移動しちゃうので、カーソル位置を保持しておく
+      const selection = window.getSelection();
+      const offset = selection.focusOffset;
+
       this.$store.commit('SET_LABEL', updateLabel);
       this.$refs.inputLabel.innerText = updateLabel;
+
+      // コミットした後、カーソル位置を戻す
+      this.$nextTick(() => {
+        if (!event.target.firstChild || event.target.firstChild.textContent.length < offset) {
+          return;
+        }
+        const range = document.createRange();
+        range.setStart(event.target.firstChild, offset);
+        range.setEnd(event.target.firstChild, offset);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      });
+    },
+    mutateComposition(event, state) {
+      this.isCompositioning = state === 'start';
+      if (state === 'end') {
+        this.changeLabel(event);
+      }
     },
   },
 };
